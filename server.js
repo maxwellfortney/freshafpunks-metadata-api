badefc9730bfbc7526eeef5dec75d9b6c62a0f35
Maxwell Fortney <maxwellfortney@gmail.com>
2021-03-19 19:22:31 -0400

    Change test gif

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const fs = require("fs");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
     res.send(
