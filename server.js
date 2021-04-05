d99214e0bcdefab4f348b5438b363c1c227592cc
Maxwell Fortney <maxwellfortney@gmail.com>
2021-04-05 11:29:20 -0400

    Adds locking

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 5001;
 const fs = require("fs");
 const path = require("path");
 
+app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -13,6 +14,44 @@ app.get("/", function (req, res) {
         "You have reached the FreshAFPunks metadata API! Query away, crypto trader!"
     );
 });
+
+let lockedIDs = [];
+
+// for (let i = 0; i < 10000; i++) {
+//     lockedIDs.push(i);
+// }
+
+app.route("/lockedIDs")
+    .get(function (req, res) {
+        res.send(JSON.stringify(lockedIDs));
+    })
+    .post(function (req, res) {
+        console.log(req.body);
+        if (req.body.length > 0) {
+            lockedIDs = [...lockedIDs, ...req.body];
+        }
+        console.log(lockedIDs);
+
+        setTimeout(() => {
+            removeLockedIDs(req.body);
+        }, 1000 * 180);
+        res.send("Locked IDs");
+    });
+
+app.post("/removeLockedIDs", (req, res) => {
+    removeLockedIDs(req.body);
+    res.send("Removed");
+});
+
+function removeLockedIDs(randomIDs) {
+    for (let i = 0; i < randomIDs.length; i++) {
+        lockedIDs = lockedIDs.filter((item) => {
+            return item !== randomIDs[i];
+        });
+    }
+
+    console.log(lockedIDs);
+}
 
 app.get("/api/token/:tokenID", function (req, res) {
     const tokenId = parseInt(req.params.tokenID).toString();
